d2638aa3f8bd319146dba2a9d33e85627ce91200
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-06-07 15:57:08 -0600

    init

--- src/Music.js
+++ src/Music.js
@@ -19,9 +19,9 @@ const Music = ({ title, artist, remove, id, editClicked }) => (
         Edit
       </Button>
       <Button.Group>
-        <Button icon="play" link href="https://www.hackreactor.com/blog/the-ultimate-songs-about-coding-playlist" rel="stylesheet" />
+        <Button icon="play" button basic color="violet" link href="https://www.hackreactor.com/blog/the-ultimate-songs-about-coding-playlist" rel="stylesheet" />
       </Button.Group>
-      <Rating icon="star" defaultRating={3} maxRating={4} />
+      <Rating icon="star" defaultRating={3} maxRating={4} size='large'/>
       
     </Table.Cell>
   </Table.Row>
